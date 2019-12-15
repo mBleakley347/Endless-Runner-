be1b001b56a058a8526f1b30b5a3312f74588395
Michael Bleakley <1013532@student.sae.edu.au>
2019-12-15 11:33:19 +1100

    Player Setup

---
+++ MightyKingdom Test/Assets/Character/SCR_PlayerControler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.iOS;
+
+public class SCR_PlayerControler : MonoBehaviour
+{
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float speed;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private bool jumped;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (jumped) return;
+        rb.AddRelativeForce(Vector2.right * speed);
+        if (Input.touchCount > 0 || Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpForce);
+        jumped = true;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Platform"))
+        {
+            jumped = false;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
