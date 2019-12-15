23254111c94385ba7332c2b2b41fa5452ee7a0bb
Michael Bleakley <1013532@student.sae.edu.au>
2019-12-15 14:43:39 +1100

    Added Score Multiplier

--- MightyKingdom Test/Assets/Platforms/SCR_PlatformManager.cs
+++ MightyKingdom Test/Assets/Platforms/SCR_PlatformManager.cs
@@ -8,10 +8,12 @@ public class SCR_PlatformManager : MonoBehaviour
     [SerializeField] private GameObject spawnPoint;
     [SerializeField] private GameObject lastPlatform;
     [SerializeField] private float verticalOffSet;
+    [SerializeField] private GameObject scoreMultiplier;
     private void Spawn()
     {
         Vector3 position = new Vector3(transform.position.x + lastPlatform.transform.lossyScale.x/8, Random.Range(0,verticalOffSet),0);
         lastPlatform = Instantiate(platforms[Random.Range(0, platforms.Length)], position, spawnPoint.transform.rotation);
+        if (Random.Range(0, 100) < 30) Instantiate(scoreMultiplier, new Vector3(position.x, position.y + 1, position.z), spawnPoint.transform.rotation);
     } 
 
     void OnTriggerExit2D(Collider2D collision)
